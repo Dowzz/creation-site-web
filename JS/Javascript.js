e38c2320514d2ad1f8314cf33c9e7abf4e06efcc
Dowzz <michel.barny@outlook.fr>
2021-08-06 01:15:45 +0200

    page et loading
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,9 +1,26 @@
 function toggleMenu(){
     var x = document.getElementById("rol-menu");
-    console.log(x);
     if(x.style.display === "block"){
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
 }
+$(function(){
+    $(document.getElementsByClassName("loader")).click(function(e){
+        e.preventDefault();
+        url=(this.href);
+        console.log(url);
+        $.get(url, function(data) {
+            $('#content').html(data);
+            if (window.matchMedia("(max-width: 1250px)").matches) {
+                document.getElementById('check')
+                .dispatchEvent(new MouseEvent('click', {shiftKey: true}));
+                
+            }
+            
+        })
+    })
+
+})
+
