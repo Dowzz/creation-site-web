08ca818102a2cdb21cb2ef1d101d76b9cd28b363
Dowzz <michel.barny@outlook.fr>
2021-08-06 00:40:14 +0200

    menu sidebar et footer
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,5 +1,5 @@
 function toggleMenu(){
-    var x = document.getElementById("mylinks");
+    var x = document.getElementById("rol-menu");
     console.log(x);
     if(x.style.display === "block"){
         x.style.display = "none";
